ce56d4602edda32e7a290ba49567149e6404c672
Mathis Dirksen-Thedens <dev@zephyrsoft.org>
2011-08-02 20:47:35 +0000

    added logging via SLF4J and LogBack
    git-svn-id: file:///home/svnroot-zephyrsoft/projects/sdb2/trunk@197 f37aa4ac-6963-46a2-a1ff-558a307fa857

---
+++ sdb2/src/java/org/zephyrsoft/sdb2/Start.java
@@ -0,0 +1,14 @@
+package org.zephyrsoft.sdb2;
+
+import org.slf4j.*;
+
+public class Start {
+	
+	private static Logger LOG = LoggerFactory.getLogger(Start.class);
+	
+	public static void main(String[] args) {
+		LOG.debug("starting application");
+		
+	}
+	
+}
