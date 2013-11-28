9d4dfbdc4ffaf2a7375802acace814ad135ca1d3
Mathis Dirksen-Thedens <dev@zephyrsoft.org>
2013-11-28 20:48:15 +0000

    SDB-15 notify the user that he has to download the JRE (if not found)
    git-svn-id: file:///home/svnroot-zephyrsoft/projects/sdb2/trunk@480 f37aa4ac-6963-46a2-a1ff-558a307fa857

--- src/dist/bin/sdb2.sh
+++ src/dist/bin/sdb2.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 BIN_DIR=$(dirname $(readlink -f "$0"))
 BASE_DIR=$(readlink -f "$BIN_DIR/..")
-java -Duser.language=de -Duser.country=DE -Dbase.dir="$BASE_DIR" -jar "$BIN_DIR/sdb2.jar" $*
+JAVA=$(which java)
+if [ -z "$JAVA" ]; then
+	echo 'Java not found, please download and install the Java Runtime Environment'
+	echo 'from here:  =>  http://www.java.com/  <='
+else
+	$JAVA -Duser.language=de -Duser.country=DE -Dbase.dir="$BASE_DIR" -jar "$BIN_DIR/sdb2.jar" $*
+fi
