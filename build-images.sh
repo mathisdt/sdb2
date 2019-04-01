bc39dd8cfcd314a3b3d76ae49ddc939d2495cee0
Mathis Dirksen-Thedens <info@zephyrsoft.org>
2019-04-01 21:13:04 +0200

    dependency updates

--- build-images.sh
+++ build-images.sh
@@ -1,18 +1,24 @@
 #!/bin/sh
 
 set -e
-#set -x
+set -x
 
-JDK=~/software/jdk11
+JDK=~/software/jdk12
 DIR=$(dirname $(readlink -f $0))
 
 ls $DIR/target/distribution/lib/*.jar | while read JAR; do
 	echo "trying to squash jar $JAR (if it is a multi-release jar) ..."
 	TMPDIR=$(mktemp -d)
 	cd $TMPDIR
-	
+
 	unzip -q $JAR
-	if [ -d META-INF/versions/11 ]; then
+	if [ -d META-INF/versions/12 ]; then
+		cp -r META-INF/versions/12/* .
+		rm -r META-INF/versions
+		rm $JAR
+		zip -rq $JAR .
+		echo "  done"
+	elif [ -d META-INF/versions/11 ]; then
 		cp -r META-INF/versions/11/* .
 		rm -r META-INF/versions
 		rm $JAR
@@ -33,7 +39,7 @@ ls $DIR/target/distribution/lib/*.jar | while read JAR; do
 	else
 		echo "  not a multi-release jar"
 	fi
-	
+
 	cd $DIR
 	rm -r $TMPDIR
 done
@@ -45,15 +51,15 @@ ls $DIR/target/distribution/lib/*.jar | while read JAR; do
 		echo "module-info.class not found, creating it"
 		TMPDIR=$(mktemp -d)
 		cd $TMPDIR
-		
-		$JDK/bin/jdeps -v --generate-module-info . --module-path $DIR/target/distribution/lib/:$DIR/target/distribution/bin/sdb2.jar:$JDK/jmods/ $JAR
+
+		$JDK/bin/jdeps -v --multi-release base --generate-module-info . --module-path $DIR/target/distribution/lib/:$DIR/target/distribution/bin/sdb2.jar:$JDK/jmods/ $JAR
 		MODULE_NAME=$(find . -type f -name module-info.java | sed -e 's#^\./##' -e 's#/.*$##')
 		echo "using name $MODULE_NAME"
 		cd $MODULE_NAME
 		unzip $JAR
-		$JDK/bin/javac -p $MODULE_NAME -d . module-info.java
-		echo zip -r $JAR module-info.class
-		
+		$JDK/bin/javac -p $MODULE_NAME:$DIR/target/distribution/lib/:$DIR/target/distribution/bin/sdb2.jar:$JDK/jmods/ -d . module-info.java
+		zip -r $JAR module-info.class
+
 		cd $DIR
 		rm -r $TMPDIR
 	else
