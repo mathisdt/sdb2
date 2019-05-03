c3992eb4e9d875dc0ec62251cea54cab777f7683
Mathis Dirksen-Thedens <info@zephyrsoft.org>
2019-05-04 01:11:31 +0200

    build distributions containing a JRE

--- build-jre-distributions.sh
+++ build-jre-distributions.sh
@@ -4,12 +4,14 @@ set -e
 
 JDK_LINUX=$JAVA_HOME
 if [ ! -d "$JDK_LINUX" -o ! -d "$JDK_LINUX/jmods" ]; then
-	echo "JAVA_HOME has to point to a directory containing a modularized Linux JDK (version 11 or higher)"
+	echo "JAVA_HOME has to point to a directory containing a Linux JDK in version 11"
 	exit 1
 fi
 
+echo "downloading a Windows JDK"
+# ATTENTION: has to match the version used on Linux -> .travis.yml
 wget -O /tmp/windows-jdk.zip 'https://api.adoptopenjdk.net/v2/binary/releases/openjdk11?openjdk_impl=hotspot&os=windows&arch=x64&release=latest&type=jdk&heap_size=normal'
-unzip -d /tmp/windows-jdk /tmp/windows-jdk.zip
+unzip -qq -d /tmp/windows-jdk /tmp/windows-jdk.zip
 JDK_WINDOWS=$(ls -d /tmp/windows-jdk/*)
 if [ ! -d "$JDK_WINDOWS" -o ! -d "$JDK_WINDOWS/jmods" ]; then
 	echo "download of Windows JDK did not work"
@@ -20,30 +22,32 @@ DIR=$(dirname $(readlink -f $0))
 
 MODULES=java.desktop,java.scripting,java.xml,java.sql,java.management,java.naming,jdk.crypto.cryptoki,jdk.crypto.ec
 
+echo "creating a Linux JRE"
 $JDK_LINUX/bin/jlink \
     --module-path $JDK_LINUX/jmods/ \
     --add-modules $MODULES \
-    --output $DIR/target/distribution-linux/jre
-$JDK_LINUX/bin/jlink \
-    --module-path $JDK_WINDOWS/jmods/ \
-    --add-modules $MODULES \
-    --output $DIR/target/distribution-windows/jre
-
-cp -r $DIR/target/distribution/* $DIR/target/distribution-linux/
-cp -r $DIR/target/distribution/* $DIR/target/distribution-windows/
-
-cat <<EOF >$DIR/target/distribution-linux/bin/sdb2.sh
+    --output $DIR/target/sdb2-bundle-linux/jre
+cp -r $DIR/target/distribution/* $DIR/target/sdb2-bundle-linux/
+cat <<EOF >$DIR/target/sdb2-bundle-linux/bin/sdb2.sh
 #!/bin/sh
 BIN_DIR=\$(dirname \$(readlink -f "\$0"))
 BASE_DIR=\$(readlink -f "\$BIN_DIR/..")
 \$BASE_DIR/jre/bin/java -Duser.language=de -Duser.country=DE -Dfile.encoding=UTF-8 -jar "\$BIN_DIR/sdb2.jar" \$*
 EOF
-chmod a+x $DIR/target/distribution-linux/bin/sdb2.sh
+chmod a+x $DIR/target/sdb2-bundle-linux/bin/sdb2.sh
 
-cat <<EOF >$DIR/target/distribution-windows/bin/sdb2.bat
+echo "creating a Windows JRE"
+$JDK_LINUX/bin/jlink \
+    --module-path $JDK_WINDOWS/jmods/ \
+    --add-modules $MODULES \
+    --output $DIR/target/sdb2-bundle-windows/jre
+cp -r $DIR/target/distribution/* $DIR/target/sdb2-bundle-windows/
+cat <<EOF >$DIR/target/sdb2-bundle-windows/bin/sdb2.bat
 ..\jre\bin\javaw.exe -Duser.language=de -Duser.country=DE -Dfile.encoding=UTF-8 sdb2.jar $*
 EOF
-rm $DIR/target/distribution-windows/bin/sdb2.sh
+rm $DIR/target/sdb2-bundle-windows/bin/sdb2.sh
 
-zip -qr $DIR/target/sdb2-bundle-for-linux.zip $DIR/target/distribution-linux/*
-zip -qr $DIR/target/sdb2-bundle-for-windows.zip $DIR/target/distribution-windows/*
+echo "packing the distributions"
+cd $DIR/target
+zip -qr $DIR/target/sdb2-bundle-linux.zip sdb2-bundle-linux
+zip -qr $DIR/target/sdb2-bundle-windows.zip sdb2-bundle-windows
