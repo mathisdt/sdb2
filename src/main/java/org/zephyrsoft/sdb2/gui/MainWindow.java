776fe769ded999e06e56d61561cc31e939a8cb03
Lenard Spiecker <spiecker@litchisystems.com>
2021-03-07 01:08:47 +0100

    fix: pom, refactoring

--- src/main/java/org/zephyrsoft/sdb2/gui/MainWindow.java
+++ src/main/java/org/zephyrsoft/sdb2/gui/MainWindow.java
@@ -66,7 +66,6 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.JPanel;
-import javax.swing.JPasswordField;
 import javax.swing.JScrollPane;
 import javax.swing.JSpinner;
 import javax.swing.JSplitPane;
