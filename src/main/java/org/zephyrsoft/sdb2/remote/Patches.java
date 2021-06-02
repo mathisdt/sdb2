a05cfe74baa3ee18f5b2c3a08cdf2e04da25afc7
Lenard Spiecker <spiecker@litchisystems.com>
2021-06-02 08:01:42 +0200

    chg: db messages to jakarta xml bind

--- src/main/java/org/zephyrsoft/sdb2/remote/Patches.java
+++ src/main/java/org/zephyrsoft/sdb2/remote/Patches.java
@@ -19,14 +19,14 @@ package org.zephyrsoft.sdb2.remote;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.xml.bind.annotation.XmlAccessOrder;
-import javax.xml.bind.annotation.XmlAccessType;
-import javax.xml.bind.annotation.XmlAccessorOrder;
-import javax.xml.bind.annotation.XmlAccessorType;
-import javax.xml.bind.annotation.XmlElement;
-import javax.xml.bind.annotation.XmlRootElement;
-
 import org.zephyrsoft.sdb2.model.Persistable;
+
+import jakarta.xml.bind.annotation.XmlAccessOrder;
+import jakarta.xml.bind.annotation.XmlAccessType;
+import jakarta.xml.bind.annotation.XmlAccessorOrder;
+import jakarta.xml.bind.annotation.XmlAccessorType;
+import jakarta.xml.bind.annotation.XmlElement;
+import jakarta.xml.bind.annotation.XmlRootElement;
 
 /**
  *
@@ -48,8 +48,8 @@ public class Patches implements Persistable {
 			patches = new ArrayList<>();
 		}
 	}
-
-    public Iterable<? extends Patch> getPatches() {
+	
+	public Iterable<? extends Patch> getPatches() {
 		return patches;
-    }
+	}
 }
